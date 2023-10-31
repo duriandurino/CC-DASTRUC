6f36af2bdb11c3c5bd7d521383a131c2bd37c3bf
Adrian <adrian.alejandrino.1115@gmail.com>
2023-10-31 19:10:49 +0800

    hehe

--- java-dastruc/merge.java
+++ java-dastruc/merge.java
@@ -2,8 +2,10 @@ public class merge {
     public static void main(String[] args){
         int[] arr = {60, 320, 7, 380, 110, 11, 10, 8};
 
-        for(int i=0; i<arr.length-1;i++){
-            
+        for(int i=1; i<arr.length-1;i++){
+            if(arr[i-1]>arr[i]){
+                
+            }
         }
     }
 }
